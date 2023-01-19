f4918244115e58adbf4bdaad6d2d7557b46d5014
matheus <matheus2002hbl@hotmail.com>
2023-01-19 19:08:05 -0300

    att 19-01-23

--- prime.py
+++ prime.py
@@ -1,30 +1,44 @@
-import mysql.connector
-import numpy as np 
-import pandas as pd
-import plotly.express as px
-
-con = mysql.connector.connect(host='localhost',db='financecontrol',user='root',password='Mhbl2@@2')
-
-cursor = con.cursor()
-
-def bd_dataframe(nome_tb, nome_lista, funcao,nomedf):
-    cursor.execute(f'SELECT * FROM {nome_tb}')
-    nome_lista = cursor.fetchall()
-    nomedf = pd.DataFrame(nome_lista, columns = ['id','id_usuario',f'{funcao}','valor','data' ])
-    fig = px.bar(nomedf, x=f"{funcao}", y='valor', color='data')
-    fig.show()
+peso = float(input("Digite o peso da porção em gramas"))
+proteina = float(input("Digite o valor da proteina"))
+carbo = float(input("Digite o valor do carbo"))
+gordura = float(input("Digite o valor da gordura"))
+peso_a_saber = float(input("Digite o peso que você quer saber"))
 
 
+def transforma_em_kg(x):
+    if x >= 1000:
+        x = x / 1000
+        return x 
+    else:
+        return x 
 
-    
-    
 
+def calcula_caloria(a,b,c):
+    calorias = ((a + b) * 4) + (c * 9)
+    calorias = calorias * (peso_a_saber / peso)
+    print(f'A porção tem {calorias} calorias')
 
 
 
 
+def transforma(gramas,macro,var,nome):
+    x = (var * macro) / gramas
+    print(f"A quantidade de {nome} em {transforma_em_kg(var)} é {x}")
 
-bd_dataframe('tb_dividas_fixas', 'dividas', 'dividas','df_dividas')
-bd_dataframe('tb_entradas_fixas', 'dividas', 'entradas','df_entradas')
+
+
+
+
+
+proteina_1 = transforma(peso,proteina,peso_a_saber,"Proteina")
+carbo_1 = transforma(peso,carbo,peso_a_saber,"Carbo")
+gordura_1 = transforma(peso,gordura,peso_a_saber,"Gordura")
+
+kcall = calcula_caloria(carbo,proteina,gordura)
+
+
+
+
+
 
 
